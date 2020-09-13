ad768f4ced1bb7a0443320661f1592c0f69594be
devanob <browndevano@gmail.com>
2020-09-13 15:35:31 -0300

    first commit

---
+++ CH_ONE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CH_ONE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
